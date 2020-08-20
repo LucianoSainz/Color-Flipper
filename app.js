6f183a8ffbbd716d9d04cd6ec90c7191bd0c7770
luciaoSainz <danielsainz28@gmail.com>
2020-08-20 19:41:19 +0200

    added function in btn

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const  colors = ["green", "red", "rgba(133,122,200)","#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomNumer = 2;
+    document.body.style.backgroundColor = colors[randomNumer];
+    color.textContent = colors[randomNumber];
+});
