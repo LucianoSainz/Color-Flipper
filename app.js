23fd7b9cd724d78bb97acb8188f1c0a984ec16e5
luciaoSainz <danielsainz28@gmail.com>
2020-08-20 19:53:42 +0200

    added function randomNumer in app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const  colors = ["green", "red", "rgba(133,122,200)","#f15025"];
+const  colors = ["green", "red", "rgba(133,122,200)",
+"#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    const randomNumer = 2;
+    const randomNumer = getRandomNumer();
+    console.log(randomNumer);
     document.body.style.backgroundColor = colors[randomNumer];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumer(){
+    return Math.floor(Math.random() * colors.length);
+} 
